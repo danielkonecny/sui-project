e4e5b8fe3c05101ff1f03201c81c5ee92fbf70c8
Daniel Konecny <dkonecny97@gmail.com>
2020-10-29 17:37:24 +0100

    Test AI uses random moves to win

--- dicewars/ai/test.py
+++ dicewars/ai/test.py
@@ -1,4 +1,7 @@
+import random
 import logging
+
+from dicewars.ai.utils import possible_attacks
 
 from dicewars.client.ai_driver import BattleCommand, EndTurnCommand
 
@@ -9,4 +12,11 @@ class AI:
         self.logger = logging.getLogger('AI')
 
     def ai_turn(self, board, nb_moves_this_turn, nb_turns_this_game, time_left):
-        return EndTurnCommand()
+
+        attacks = list(possible_attacks(board, self.player_name))
+        if attacks:
+            source, target = random.choice(attacks)
+            return BattleCommand(source.get_name(), target.get_name())
+        else:
+            self.logger.debug("No more possible turns.")
+            return EndTurnCommand()
