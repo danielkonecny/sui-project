3da9bf8a95a4149fb4b0cd0d163c05a76a9b3084
Daniel Konecny <dkonecny97@gmail.com>
2020-12-09 15:51:00 +0100

    Add other RL tests

---
+++ gym-test/tf-cartpole.py
@@ -0,0 +1,83 @@
+from __future__ import absolute_import, division, print_function
+
+import base64
+import imageio
+import IPython
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import PIL.Image
+import pyvirtualdisplay
+
+import tensorflow as tf
+
+from tf_agents.agents.dqn import dqn_agent
+from tf_agents.drivers import dynamic_step_driver
+from tf_agents.environments import suite_gym
+from tf_agents.environments import tf_py_environment
+from tf_agents.eval import metric_utils
+from tf_agents.metrics import tf_metrics
+from tf_agents.networks import q_network
+from tf_agents.policies import random_tf_policy
+from tf_agents.replay_buffers import tf_uniform_replay_buffer
+from tf_agents.trajectories import trajectory
+from tf_agents.utils import common
+
+tf.compat.v1.enable_v2_behavior()
+
+# Set up a virtual display for rendering OpenAI gym environments.
+display = pyvirtualdisplay.Display(visible=0, size=(1400, 900)).start()
+
+num_iterations = 20000 # @param {type:"integer"}
+
+initial_collect_steps = 100  # @param {type:"integer"}
+collect_steps_per_iteration = 1  # @param {type:"integer"}
+replay_buffer_max_length = 100000  # @param {type:"integer"}
+
+batch_size = 64  # @param {type:"integer"}
+learning_rate = 1e-3  # @param {type:"number"}
+log_interval = 200  # @param {type:"integer"}
+
+num_eval_episodes = 10  # @param {type:"integer"}
+eval_interval = 1000  # @param {type:"integer"}
+
+
+env_name = 'CartPole-v0'
+env = suite_gym.load(env_name)
+
+env.reset()
+PIL.Image.fromarray(env.render())
+
+print('Observation Spec:')
+print(env.time_step_spec().observation)
+print('Reward Spec:')
+print(env.time_step_spec().reward)
+print('Action Spec:')
+print(env.action_spec())
+
+
+time_step = env.reset()
+print('Time step:')
+print(time_step)
+
+action = np.array(1, dtype=np.int32)
+
+next_time_step = env.step(action)
+print('Next time step:')
+print(next_time_step)
+
+
+train_py_env = suite_gym.load(env_name)
+eval_py_env = suite_gym.load(env_name)
+
+
+train_env = tf_py_environment.TFPyEnvironment(train_py_env)
+eval_env = tf_py_environment.TFPyEnvironment(eval_py_env)
+
+
+fc_layer_params = (100,)
+
+q_net = q_network.QNetwork(
+    train_env.observation_spec(),
+    train_env.action_spec(),
+    fc_layer_params=fc_layer_params)
